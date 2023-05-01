65007b83445797bff692cd05bb49bd0eb7ddd6c3
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-01 21:44:11 +0600

    recipe data insert done

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const chef = require('./data/chef.json')
 app.get('/', (req, res) => {
     res.send('your server ins running')
 })
-// for chef port
+// for chef port start
 app.get('/chef', (req, res) => {
     res.send(chef);
 })
-// for chef port
+// for chef port end
 
 app.listen(port, () => {
     console.log(`your api is running on : ${port}`);
