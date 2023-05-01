5eacbc262f6634d5dd035cafdeac09a732503517
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-01 22:31:15 +0600

    cors policy done

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chef = require('./data/chef.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('your server ins running')
