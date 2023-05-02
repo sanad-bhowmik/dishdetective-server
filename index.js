2f2e203c7aab9453762b99753cba17cbf41e7ae2
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-02 12:20:55 +0600

    recipe data show done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chef = require('./data/chef.json')
+const recipe = require('./data/recipe.json')
 
 app.use(cors());
 
@@ -15,6 +16,12 @@ app.get('/chef', (req, res) => {
     res.send(chef);
 })
 // for chef port end
+
+// recipe
+app.get('/recipe', (req, res) => {
+    res.send(recipe);
+})
+// recipe
 
 app.listen(port, () => {
     console.log(`your api is running on : ${port}`);
