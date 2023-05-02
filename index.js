76c816a52393d1a464c2e779c147b402afb25373
sanad-bhowmik <sanadbhowmik93@gmail.com>
2023-05-02 13:51:56 +0600

    separate by data id done

--- index.js
+++ index.js
@@ -15,11 +15,28 @@ app.get('/', (req, res) => {
 app.get('/chef', (req, res) => {
     res.send(chef);
 })
+
 // for chef port end
 
 // recipe
 app.get('/recipe', (req, res) => {
     res.send(recipe);
+})
+
+app.get('/recipe/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedRecipe = recipe.find(n => n._id === id);
+    res.send(selectedRecipe)
+})
+
+
+app.get('/chef/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const chefRecipe = recipe.filter(n => parseInt(n.chef_id) === id);
+    res.send(chefRecipe)
+
 })
 // recipe
 
